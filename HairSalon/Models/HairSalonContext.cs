fbedec827b091a9d44cbd5ad71a5902f08c37687
Sean Downs <spdowns@protonmail.com>
2020-07-31 14:20:54 -0700

    rereading lessons to figure out how to print details from clients in the stylists details page

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -6,7 +6,10 @@ namespace HairSalon.Models
   {
     public virtual DbSet<Stylist> Stylists { get; set; }
     public DbSet<Client> Clients { get; set; }
-
     public HairSalonContext(DbContextOptions options) : base(options) {}
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //   optionsBuilder.UseLazyLoadingProxies();
+    // }
   }
 }
