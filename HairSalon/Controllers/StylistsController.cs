304b99e4c99371b5f56076bbe44fa41288fe07ac
Sean Downs <spdowns@protonmail.com>
2020-07-31 09:29:44 -0700

    add the ActionResult Index to the stylistscontroller and build out index in Views/Stylists

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -3,3 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.HairSalon.ToList();
+      return View(model);
+    }
+  }
+}
